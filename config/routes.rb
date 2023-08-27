364b1141d2e38b97ead0cbb9054eca382dae3657
Dylan Palmboom <nofxboy1234@gmail.com>
2023-08-27 09:33:24 +0200

    Update routes file

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: 'posts#index'
+  root 'posts#index'
   resources :posts, only: [:new, :create, :index]
 end
